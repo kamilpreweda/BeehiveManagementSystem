d682c1c722be2020b4c643ec84f350e08984f2ac
Kamil Preweda <preweda.kamil@o2.pl>
2022-05-30 15:53:50 +0200

    Created Bee base class

---
+++ BeehiveManagementSystem/Bee.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeehiveManagementSystem
+{
+    public class Bee
+    {
+
+        public virtual float CostPerShift { get; }
+        public string Job { get; private set; }
+        public Bee(string job)
+        {
+            Job = job;
+        }
+
+        public void WorkTheNextShift()
+        {
+            if (HoneyVault.ConsumeHoney(CostPerShift))
+            {
+                DoJob();
+            }
+        }
+
+        protected virtual void DoJob() { /* przesłaniana w podklasie */ }
+    }
+}
